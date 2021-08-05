c072da90ff1a454b5fcd5ab597b93ccd0fc63d37
Gary Ng <garyng@garys-mbp.attlocal.net>
2021-08-05 16:31:16 -0400

    setup game windows

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import pygame
 import random
 import os
 
+FPS = 60
+WHITE = (255, 255, 255)
+WIDTH = 500
+HEIGHT = 600
 
 #init and windeos
 pygame.init()
-screen = pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
 running = True
 
 # loop
 while running:
+    clock.tick(FPS)
     # get input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # update game
+    # update game
 
-        # display game
+    # display game
+    screen.fill(WHITE)
+    pygame.display.update()
+
 pygame.quit()
