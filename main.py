9e0860b84ba1e2c3afec27d0797657c24bdff54d
Gary Ng <garyng@garys-mbp.attlocal.net>
2021-08-05 16:47:50 -0400

    add player class

--- main.py
+++ main.py
@@ -3,14 +3,38 @@ import random
 import os
 
 FPS = 60
-WHITE = (255, 255, 255)
 WIDTH = 500
 HEIGHT = 600
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
 
 #init and windeos
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Space Invader')
 clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = 200
+        self.rect.y = 200
+
+    def update(self):
+        self.rect.x += 2
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
 
 running = True
 
@@ -21,10 +45,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     # update game
+    all_sprites.update()
 
     # display game
     screen.fill(WHITE)
+    all_sprites.draw(screen)
     pygame.display.update()
 
 pygame.quit()
